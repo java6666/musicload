07f5e00c654987f751573055e494d204aa7e5470
张津瑞 <369435967@qq.com>
2017-04-06 15:38:45 +0800

    create

---
+++ src/main/java/musicload/controller/UserController.java
@@ -0,0 +1,7 @@
+package musicload.controller;
+
+/**
+ * Created by Administrator on 2017/4/6.
+ */
+public class UserController {
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2017/4/6.
+ */
+public class Test {
+}
